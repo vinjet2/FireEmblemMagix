2931a8287d04ba9001fe58f10b478fd9ba4a71b1
Vincent <vinjet2@gmail.com>
2022-09-08 15:45:37 -0400

    Ajout du Squelette du Projet

---
+++ lobby.php
@@ -0,0 +1,11 @@
+<?php
+    require_once("action/lobbyAction.php");
+
+    $action = new lobbyAction();
+    $data = $action->execute();
+
+    require_once("partial/header.php");
+?>
+
+<?php
+    require_once("partial/footer.php");
